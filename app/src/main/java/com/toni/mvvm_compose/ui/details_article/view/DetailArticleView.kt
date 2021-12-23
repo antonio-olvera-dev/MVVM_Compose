7b9b56b56c2427dab5ee9a458643a58fae7401a9
Antonio Olvera <antonio.olvera.dev@gmail.com>
2021-12-23 18:22:26 +0100

    build DetailsArticleView

--- app/src/main/java/com/toni/mvvm_compose/ui/details_article/view/DetailArticleView.kt
+++ app/src/main/java/com/toni/mvvm_compose/ui/details_article/view/DetailArticleView.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.toni.mvvm_compose.data.article.models.ArticleGetResponse
 import com.toni.mvvm_compose.shared.Navigate
 
@@ -22,21 +24,49 @@ fun DetailsArticleView(ctx: Context) {
 
 private class DetailsArticleComponents(val ctx: Context) {
 
-
     val article: ArticleGetResponse = Navigate.Bundles.DetailsArticle.articleGetResponse!!
 
     @Composable
     fun Body() {
 
-        Column(Modifier.fillMaxSize()) {
-            Text(text = "${article.explanation}")
+        Column(Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+        ) {
+            Spacer(modifier = Modifier.padding(bottom = 10.dp))
+            Image()
             Spacer(modifier = Modifier.padding(bottom = 20.dp))
-            Button(onClick = {
-                Navigate.toHome()
-            }) {
-                Text(text = "Go to Home")
-            }
+            Title()
+            Spacer(modifier = Modifier.padding(bottom = 20.dp))
+            Description()
         }
+    }
+
+
+    @Composable
+    private fun Image() {
+        Text(
+            text = article.hdUrl,
+            fontSize = 16.sp,
+        )
+
+    }
+
+    @Composable
+    private fun Title() {
+        Text(
+            text = article.title,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+    @Composable
+    private fun Description() {
+        Text(
+            text = article.explanation,
+            fontSize = 16.sp,
+        )
     }
 
 }
